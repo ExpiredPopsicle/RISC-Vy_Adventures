193689688946c5029e63f29f8dbb6e801967f981
Kiri <expiredpopsicle@gmail.com>
2023-01-01 18:09:38 -0800

    Ending stream.

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <assert.h>
-
+#include <malloc.h>
 
 struct CPUState
 {
@@ -64,6 +64,26 @@ void dump_state(const struct CPUState *state)
 
 void (*CPUInstruction)(uint32_t instruction);
 
+uint32_t sign_extend(uint32_t value, uint32_t length)
+{
+    uint32_t extend_bit = (value >> (length - 1)) & 1;
+    uint32_t extension = 0xffffffff << length;
+
+    printf("value:      %.32b\n", value);
+    printf("extend_bit: %.32b\n", extend_bit);
+    printf("extension:  %.32b\n", extension);
+
+    uint32_t extend_or_not[2] = {
+        0,
+        extension
+    };
+
+    uint32_t result = value | extend_or_not[extend_bit];
+
+    printf("result:     %.32b\n", result);
+    return result;
+}
+
 void cpu_instruction_op_imm(
     struct CPUState *state,
     uint32_t instruction)
@@ -76,7 +96,7 @@ void cpu_instruction_op_imm(
 
     imm    = (instruction & 0b11111111111100000000000000000000) >> 20;
     rs1    = (instruction & 0b00000000000011111000000000000000) >> 15;
-    funct3 = (instruction & 0b00000000000000000111000000000000) >> 13;
+    funct3 = (instruction & 0b00000000000000000111000000000000) >> 12;
     rd     = (instruction & 0b00000000000000000000111110000000) >>  7;
     opcode = (instruction & 0b00000000000000000000000001111111) >>  0;
 
@@ -87,36 +107,63 @@ void cpu_instruction_op_imm(
     printf("rs1:         %b (%u)\n", rs1, rs1);
     printf("imm:         %b (%u)\n", imm, imm);
 
+    uint32_t imm_value_signextended =
+        sign_extend(imm, 12);
+
+    uint32_t rs1_value =
+        read_register(state, rs1);
+
+    printf("imm_value_signextended: %b (%u)\n",
+        imm_value_signextended, imm_value_signextended);
 
     switch(funct3)
     {
-        case 0b000:
-            // ADDI
+        case 0b000: // ADDI
+
             write_register(
                 state,
                 rd,
-                imm + read_register(
-                    state, rs1));
+                imm_value_signextended + rs1_value);
             break;
 
-        case 0b010:
-            // SLTI
+        case 0b010: // SLTI
+
+            write_register(
+                state,
+                rd,
+                (int32_t)imm_value_signextended >= (int32_t)rs1_value);
             break;
 
-        case 0b011:
-            // SLTIU
+        case 0b011: // SLTIU
+
+            write_register(
+                state,
+                rd,
+                imm_value_signextended >= rs1_value);
             break;
 
-        case 0b100:
-            // XORI
+        case 0b100: // XORI
+
+            write_register(
+                state,
+                rd,
+                imm_value_signextended ^ rs1_value);
             break;
 
-        case 0b110:
-            // ORI
+        case 0b110: // ORI
+
+            write_register(
+                state,
+                rd,
+                imm_value_signextended | rs1_value);
             break;
 
-        case 0b111:
-            // ANDI
+        case 0b111: // ANDI
+
+            write_register(
+                state,
+                rd,
+                imm_value_signextended & rs1_value);
             break;
 
         case 0b001:
@@ -125,14 +172,32 @@ void cpu_instruction_op_imm(
             // SRLI/SRAI
         {
             uint32_t shamt = imm & 0b11111;
+
+            uint32_t shift_result;
+            if(funct3 == 1) {
+                shift_result = rs1_value << shamt;
+            } else {
+                shift_result = rs1_value >> shamt;
+            }
+
+            printf("imm:       %b\n", imm);
+            printf("rs1_value: %b\n", rs1_value);
+            if((imm & 0b10000000000) && (rs1_value & 1)) {
+
+                uint32_t arithmetic_extension = 0xffffffff >> 32 - shamt;
+                printf(
+                    "kjsdcnjksdfcnjksdcnjkdcnaskdjcnasdc %b\n",
+                    arithmetic_extension);
+
+                shift_result |= arithmetic_extension;
+            }
+
+            write_register(state, rd, shift_result);
+
         }
             break;
     }
 
-
-    // DECODE_I_TYPE();
-
-    // 0b0010011
 }
 
 void execute_instruction(
@@ -150,27 +215,79 @@ void execute_instruction(
     }
 }
 
+void *load_file(
+    const char *filename,
+    size_t *out_length)
+{
+    FILE *in_file = fopen(filename, "rb");
+    assert(in_file); // TODO: Actual error.
+
+    size_t input_buffer_size = 256;
+    char *output_buffer = (char *)malloc(input_buffer_size);
+    assert(output_buffer); // TODO: Actual error.
+
+    int in_byte;
+    size_t write_ptr = 0;
+    while((in_byte = fgetc(in_file)) != EOF) {
+
+        output_buffer[write_ptr++] = in_byte;
+
+         if(write_ptr >= input_buffer_size) {
+
+            size_t old_input_buffer_size = input_buffer_size;
+            void *old_buffer = output_buffer;
+
+            input_buffer_size <<= 1;
+
+            // TODO: Actual error.
+            assert(input_buffer_size > old_input_buffer_size);
+
+            output_buffer = realloc(
+                output_buffer, input_buffer_size);
+
+            assert(output_buffer); // TODO: Actual error.
+
+            if(!output_buffer) {
+                free(old_buffer);
+            }
+
+        }
+    }
+
+    *out_length = write_ptr;
+    fclose(in_file);
+    return output_buffer;
+}
+
 int main(int argc, char *argv[])
 {
-    uint32_t memory[] = {
-        0x00000013, // nop
-        0x00308093, // addi    x1, x1, 3
-    };
+    // uint32_t memory[] = {
+    //     0x00000013, // nop
+    //     0x00308093, // addi    x1, x1, 3
+    // };
+
+    size_t mem_size = 0;
+    void *memory = load_file("test/test.asm.bin", &mem_size);
+    uint32_t program_length = mem_size / 4;
+
+    printf("program_length: %u\n", program_length);
 
     struct CPUState state;
     memset(&state, 0, sizeof(state));
 
-    while(state.registers.program_counter < sizeof(memory) / sizeof(memory[0])) {
-        uint32_t the_instruction = memory[state.registers.program_counter];
+    while(state.registers.program_counter < program_length) {
+        uint32_t the_instruction = ((uint32_t*)memory)[state.registers.program_counter];
         cpu_instruction_op_imm(&state, the_instruction);
         state.registers.program_counter += 1;
     }
 
-    // the_instruction = 0x00308093;
-
-    // cpu_instruction_op_imm(&state, the_instruction);
-
     dump_state(&state);
+
+    free(memory);
+
+
+    uint32_t sign_extend_me = 0b0001011;
+    sign_extend(sign_extend_me, 4);
 
     return 0;
 }
--- test/test.asm
+++ test/test.asm
@@ -1,3 +1,4 @@
+/*
     nop
     addi    x1, x1, 3
     addi    x1, x1, 3
@@ -6,3 +7,28 @@
     addi    x1, x1, 3
     addi    x1, x1, 3
     addi    ra, ra, 3
+
+    addi    x1, x1, -1
+*/
+/*
+    sltiu    x3, x1, -1
+    slti     x2, x1, -1
+    */
+
+
+
+    addi x1, x1, 0x010
+
+    xori x4, x1, -1
+    ori  x5, x1, 0x100
+
+    andi x6, x1, 0x111
+    andi x7, x1, 0x101
+
+
+
+    slli x8, x1, 0x1
+
+
+    xori x1, x1, 0x011
+    srai x9, x1, 0x5
